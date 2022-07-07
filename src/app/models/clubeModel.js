e78bd8095a7daf557f67ec6cf5ec7cd1ce3e9c22
mineironovale <augustomessias@pm.me>
2022-07-07 20:56:08 -0300

    Criar arquivo clubeModel.js

---
+++ src/app/models/clubeModel.js
@@ -0,0 +1,8 @@
+function clubeModel(nome, anoDeFundacao, estadio, cores) {
+    this.nome = nome;
+    this.anoDeFundacao = anoDeFundacao;
+    this.estadio = estadio;
+    this.cores = cores;
+}
+
+module.exports = clubeModel;
