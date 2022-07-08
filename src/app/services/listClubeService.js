bb31eb1947d63db7138ffd36a696fedc02bbf973
mineironovale <augustomessias@pm.me>
2022-07-08 12:02:33 -0300

    Alterar arquivo clubeService.js para listClubeService.js com a criação do método listClubeData

---
+++ src/app/services/listClubeService.js
@@ -0,0 +1,49 @@
+const clubeModel = require("../models/clubeModel");
+
+const listClubeService = {
+    listClubeService: () => {
+        const clube1 = new clubeModel(
+            1,
+            "Arsenal",
+            1886,
+            "Emirates Stadium",
+            ["branco", "vermelho"],
+            "Mikel Arteta",
+            [
+                "Ramsdale",
+                "Tavares",
+                "Gabriel",
+                "Holding",
+                "Soares",
+                "Elneny",
+                "Odegaard",
+                "Xhaka",
+                "Martinelli",
+                "Nketiah",
+                "Saka"
+            ]
+        )
+        const clube2 = new clubeModel(
+            2,
+            "Fulham",
+            1890,
+            "Estádio do Fulham",
+            ["branco", "preto"],
+            "Buffon",
+            [
+                "Peter Cech",
+                "Lampard",
+                "Van Bommel"
+            ]
+        )
+
+        return [clube1, clube2];
+    },
+    listClubeData: (clubeNome) => {
+        const clubeList = clubeService.listClubeService();
+        const clube = clubeList.find(item => item.nome === nome);
+        return clube;
+    }
+}
+
+module.exports = clubeService;
